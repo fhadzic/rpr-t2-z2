f576cabbcbd93f3cd08ef96345f3c42cc784f029
Ferid <fhadzic1@etf.usna.ba>
2018-10-25 10:31:58 +0200

    Prvi commit

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,33 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double pocetak;
+    private double kraj;
+    boolean daLiPripadaPocetak;
+    boolean daLiPripadaKraj;
 
+
+    public Interval(double pocetak, double kraj, boolean daLiPripadaPocetak, boolean daLiPripadaKraj) {
+
+        if( pocetak > kraj ) throw IllegalArgumentException
+
+        this.pocetak = pocetak;
+        this.kraj = kraj;
+
+    }
+
+    public Interval() {
+
+        this.pocetak = 0;
+        this.kraj = 0;
+
+    }
+
+    public isNull(){
+        if(pocetak == 0 && kraj == 0){
+            return true;
+        }
+        return false;
+    }
 
 }
