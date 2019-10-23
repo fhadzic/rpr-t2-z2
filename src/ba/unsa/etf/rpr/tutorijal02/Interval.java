8730857ec299167e6dd18d3fd5b47e40a62aafdd
fhadzic <fhadzic1@etf.unsa.ba>
2019-10-23 13:56:34 +0200

    Podesio da prođu svi testovi

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -9,36 +9,77 @@ public class Interval {
 
     public Interval(double pocetak, double kraj, boolean daLiPripadaPocetak, boolean daLiPripadaKraj) {
 
-        if( pocetak > kraj ) throw new IllegalArgumentException("");
+        if( pocetak > kraj ) throw new IllegalArgumentException("Početna tačka, veća od krajnje!");
 
         this.pocetak = pocetak;
         this.kraj = kraj;
+        this.daLiPripadaPocetak = daLiPripadaPocetak;
+        this.daLiPripadaKraj = daLiPripadaKraj;
 
     }
 
     public Interval() {
-
         this.pocetak = 0;
         this.kraj = 0;
-
+        this.daLiPripadaPocetak = false;
+        this.daLiPripadaKraj = false;
     }
 
     public boolean isNull(){
-        if(pocetak == 0 && kraj == 0){
+        if(pocetak == 0 && kraj == 0 && !daLiPripadaPocetak && !daLiPripadaKraj){
             return true;
         }
         return false;
     }
 // moje
     public boolean isIn(double v) {
+
+        if( (v-pocetak > 0.000001 || (v-pocetak == 0 && daLiPripadaPocetak)) && ( kraj-v >0.000001 || (kraj-v == 0 && daLiPripadaKraj) ) ){
+            return true;
+        }
+
         return false;
     }
 
     public Interval intersect(Interval interval) {
-        return null;
+        Interval result = intersect(this, interval);
+        return result;
     }
 
     public static Interval intersect(Interval i, Interval i2) {
+        if( i.pocetak < i2.pocetak && i.kraj < i2.kraj){
+            return new Interval(i2.pocetak, i.kraj, i2.daLiPripadaPocetak, i.daLiPripadaKraj);
+        }else if(i.pocetak > i2.pocetak && i.kraj > i2.kraj){
+            return new Interval(i.pocetak, i2.kraj, i.daLiPripadaPocetak, i2.daLiPripadaKraj);
+        }else if(i.pocetak < i2.pocetak && i.kraj > i2.kraj){
+            return new Interval(i2.pocetak, i2.kraj, i2.daLiPripadaPocetak, i2.daLiPripadaKraj);
+        }else if(i.pocetak > i2.pocetak && i.kraj < i2.kraj){
+            return new Interval(i.pocetak, i.kraj, i.daLiPripadaPocetak, i.daLiPripadaKraj);
+        }
         return null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Interval i2 = (Interval) obj;
+        if(pocetak - i2.pocetak < 0.000001 && kraj -i2.kraj < 0.0000001 && daLiPripadaPocetak == i2.daLiPripadaPocetak && daLiPripadaKraj == i2.daLiPripadaKraj){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        if(daLiPripadaPocetak && daLiPripadaKraj){
+            return String.format("[%.1f,%.1f]",pocetak, kraj);
+        }else if(daLiPripadaPocetak){
+            return String.format("[%.1f,%.1f)",pocetak, kraj);
+        }else if(daLiPripadaKraj){
+            return String.format("(%.1f,%.1f]",pocetak, kraj);
+        }else {
+            if(pocetak != 0 || kraj != 0)
+                return String.format("(%.1f,%.1f)", pocetak, kraj);
+        }
+        return String.format("()", pocetak, kraj);
     }
 }
